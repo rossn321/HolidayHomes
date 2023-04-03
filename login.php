703186bdbaba9d353a685f953f1d81f83b3fd2ca
rossn321 <103645097+rossn321@users.noreply.github.com>
2023-04-03 14:08:18 +0100

    Update and rename login.html to login.php

--- login.php
+++ login.php
@@ -10,43 +10,32 @@
 
 
 <body>
-<?php
+    <?php
     require("database.php");
     session_start();
-    //When form is submitted, check & create user session 
-    if (isset($_POST['UserName'])){ 
+    // When form is submitted, check & create user session 
+    if (isset($_POST['submit']) && isset($_POST['UserName'])){ 
       $username = stripslashes($_REQUEST['UserName']); // Removes blackslashes
       $username = mysqli_real_escape_string($con, $username);
 
       $password = stripslashes($_REQUEST['password']);
       $password = mysqli_real_escape_string($con, $password);
 
-      //Check if user exists in database 
+      // Check if user exists in database 
       $query = "SELECT * FROM  Users WHERE UserName = '$username' AND password='" . md5($password) . "' ";
       $result = mysqli_query($con, $query) or die(mysqli_error($con));
       $rows = mysqli_num_rows($result);
       // if result is true/1
       if ($rows == 1) { 
-        // Check if user an Admin
-        $query = "SELECT * FROM Users WHERE UserName = '$username'";
-        $result = mysqli_query($con, $query) or die(mysqli_error($con));
-        $rows = mysqli_num_rows($result);
-       // If row returns 1 then log admin user into admin dashboard
-        if ($rows == 1){ 
-          $_SESSION['UserName'] = $username;
-          // Redirect the user to the home page 
-          header("Location: index.html");
-          // Else if rows returns 0 then log user into normal user page
-        } 
-        // Display error message
+        $_SESSION['UserName'] = $username;
+        // Redirect the user to the home page 
+        header("Location: index.html");
       } else { 
-        echo "<div class='form'>
-              <h3> Incorrect UserName/Password.</h3><br/>
-              <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
-              </div>";              
+        echo "Incorrect username or password.";              
       }
     } else { 
-     ?>
+?>
+
 	<nav class="sizeNav">
 		<div class="flexBetween">
 			<div><img src="./img/logonoback.png" alt="logo" width="187px" height="31px"></div>
@@ -66,12 +55,12 @@
       <h1>Login</h1>
       <form method="post">
         <div class="txt_field">
-          <input type="text" required>
+            <input type="text" name="UserName" required>
           <span></span>
           <label>Username</label>
         </div>
         <div class="txt_field">
-          <input type="password" required>
+            <input type="password" name="password" required>
           <span></span>
           <label>Password</label>
         </div>
